8a895a6d7eca15f259c21aceeb54ddff4de2d441
alexmilowski <alexmilowski@33ab2c10-8d2a-0410-a5f1-c19a52e2b515>
2011-10-11 20:41:36 +0000

    removed window size setting (default to current window size)

--- poster/src/content/overlay.js
+++ poster/src/content/overlay.js
@@ -1,6 +1,6 @@
 function showPoster() {
    window.open(
-      'chrome://poster/content/poster-window.xul','poster-'+(new Date()).getTime(),'centerscreen,resizable,width=550,height=700'
+      'chrome://poster/content/poster-window.xul','poster-'+(new Date()).getTime(),'centerscreen,resizable'
    );
 }
 
