91295a678bdf3d10429d5ab6bc5f0bd837f77c63
alexmilowski <alexmilowski@33ab2c10-8d2a-0410-a5f1-c19a52e2b515>
2011-10-11 19:21:16 +0000

    added first time page

--- poster/src/content/overlay.js
+++ poster/src/content/overlay.js
@@ -3,3 +3,27 @@ function showPoster() {
       'chrome://poster/content/poster-window.xul','poster-'+(new Date()).getTime(),'centerscreen,resizable,width=550,height=700'
    );
 }
+
+var preferencesService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.poster.");
+if (preferencesService) {
+   var loaded = false;
+   try {
+      loaded = preferencesService.getBoolPref("loaded");
+   } catch (ex) {
+      // no preference
+   }
+   if (!loaded) {
+      if (gBrowser) {
+         setTimeout(function() {
+            if (gBrowser.contentDocument.location!="chrome://poster/content/poster-window.xul") {
+               if (gBrowser.contentDocument.location=="about:blank") {
+                  gBrowser.selectedBrowser.loadURI("chrome://poster/content/first-time.xhtml")
+               } else {
+                  gBrowser.selectedTab = gBrowser.addTab("chrome://poster/content/first-time.xhtml");
+               }
+            }
+         },500);
+      }
+   }
+   preferencesService.setBoolPref("loaded",true);
+}
